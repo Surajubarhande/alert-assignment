2a4e84db0b4ac9e8f81211561e0852669f51adb0
Surajubarhande <surajalk1996@gmail.com>
2022-11-13 13:54:48 +0530

    added by suraj

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 dob = prompt('Enter your birth year');
-cyear = prompt('Enter current year');
+// curret year mde 2022 jar takl tr direct age display honar
+cyear = prompt('Enter your curret year');
 age = cyear - dob;
 
 alert('your age is=' + age + 'years')
